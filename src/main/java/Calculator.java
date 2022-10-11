a9fb0567c802110f6f8a0b2ce52815bcec19dc87
Tyler Mansell <tyler.mansell20@gmail.com>
2022-10-10 20:49:12 -0600

    fixed tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Random;
 
 class Calculator {
 
@@ -39,7 +42,12 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        if (n <= 0) {
+            return 0;
+        } if (n <= 2) {
+            return 1;
+        }
+        return fibonacciNumberFinder(n-1) + fibonacciNumberFinder(n-2);
     }
 
 
@@ -51,7 +59,7 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return null;
+        return Integer.toBinaryString(n);
     }
 
     /*
@@ -63,9 +71,6 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+        return n + Long.toString(new Date().getTime()) + Double.toString(Math.random());
     }
-
-
 }
